8782c3e9f149afbda4e7e6862d79b94e873dc4fd
Adrian Castillo <adrianhumbertocastillo@gmail.com>
2019-07-17 16:03:17 -0600

    Modificados archivos css y js

--- Resources/js/menu.js
+++ Resources/js/menu.js
@@ -1,25 +1,65 @@
-$(document).ready(main);
+ $(document).ready(function(){
+ 	Menu();
+ 	Barras();
+ });
 
 var contador = 1;
 
-function main(){
+function Menu(){
 	$('.menu_bar').click(function(){
-
 		if(contador == 1){
 			$('nav').animate({
 				left: '0'
-			});
-
-			
+			});			
 			contador = 0;
 		}else{
 			contador = 1;
 			$('nav').animate({
 				left: '-100%'
 			});
-
-
 		}
 	});
+}
 
+function Barras(){
+	 $("#Barra1").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
+    $("#Barra2").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
+    $("#Barra3").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
+    $("#Barra4").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
+    $("#Barra5").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
+    $("#Barra6").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
+    $("#Barra7").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
+    $("#Barra8").hover(function(){
+        $(this).addClass("progress-striped active");}, 
+        function(){
+        $(this).removeClass("progress-striped active");
+    });
 }
