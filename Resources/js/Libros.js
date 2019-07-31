023e8599738e8f4cad4aeb40599a0816d86f925f
Adrian Castillo <adrianhumbertocastillo@gmail.com>
2019-07-30 22:44:47 -0600

    Agregada libreria para tooltips y modificados archivos de catalogo de libros

--- Resources/js/Libros.js
+++ Resources/js/Libros.js
@@ -1,13 +1,13 @@
 
 var vm = new Vue({
-  el: '#Catalogo', 
+  el: 'main', 
   data: {
-    libros: [
+    listado: [
       {titulo: '1984', autor: 'George Orwell', paginas: 432, isbn: '978-8490328248', imagen: 'Resources/img/Libros/978-8490328248.jpg'},
       {titulo: 'Rebelion en la granja', autor: 'George Orwell', paginas: 144, isbn: '9788499890951', imagen: 'Resources/img/Libros/9788499890951.jpg'},
-      ],
-   
+    ],    
   },
 
-  
+
 })
+
