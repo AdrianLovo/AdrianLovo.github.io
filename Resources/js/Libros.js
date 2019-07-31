5e1e709b4f73136a4f252ae94121330aee455843
Adrian Castillo <adrianhumbertocastillo@gmail.com>
2019-07-30 18:46:03 -0600

    Eliminados elementos css innecesarios y Agregadas imagenes de tecnologias

--- Resources/js/Libros.js
+++ Resources/js/Libros.js
@@ -2,19 +2,12 @@
 var vm = new Vue({
   el: '#Catalogo', 
   data: {
-    listado: [
-      {titulo: '1984', autor: 'George Orwell', paginas: 432, isbn: '978-8490328248', imagen: '../../Resources/img/Libros/978-8490328248.jpg'},
-      {titulo: 'Rebelion en la granja', autor: 'George Orwell', paginas: 144, isbn: '9788499890951', imagen: '../../Resources/img/Libros/9788499890951.jpg'},
+    libros: [
+      {titulo: '1984', autor: 'George Orwell', paginas: 432, isbn: '978-8490328248', imagen: 'Resources/img/Libros/978-8490328248.jpg'},
+      {titulo: 'Rebelion en la granja', autor: 'George Orwell', paginas: 144, isbn: '9788499890951', imagen: 'Resources/img/Libros/9788499890951.jpg'},
       ],
-    busqueda: '',
-    alto: 200,
-    ancho: 200,
+   
   },
 
-  computed: {
-    buscarPorTitulo() { 
-      return this.listado.filter((busqueda) => busqueda.titulo.includes(this.busqueda))
-    },
- },
- 
+  
 })
--- Resources/js/index.js
+++ Resources/js/index.js
@@ -22,50 +22,12 @@ function Menu(){
 }
 
 function Barras(){
-	 $("#Barra1").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra2").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra3").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra4").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra5").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra6").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra7").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra8").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
-    $("#Barra9").hover(function(){
-        $(this).addClass("progress-striped active");}, 
-        function(){
-        $(this).removeClass("progress-striped active");
-    });
+    for(i =1; i <10; i++){
+        $("#Barra"+i).hover(function(){
+            $(this).addClass("progress-striped active");}, 
+            function(){
+            $(this).removeClass("progress-striped active");
+        });
+    }
 }
 
