c340293d005cbba2e4f39ff4b001161aa90b92f3
AdrianLovo <adrianhumbertocastillo@gmail.com>
2019-08-07 17:51:01 -0600

    modificado estilos y agregada pagina sobre libros

--- Resources/js/Libros.js
+++ Resources/js/Libros.js
@@ -3,8 +3,8 @@ var vm = new Vue({
   el: 'main', 
   data: {
     listado: [
-      {titulo: '1984', autor: 'George Orwell', paginas: 432, isbn: '978-8490328248', imagen: 'Resources/img/Libros/978-8490328248.jpg'},
-      {titulo: 'Rebelion en la granja', autor: 'George Orwell', paginas: 144, isbn: '9788499890951', imagen: 'Resources/img/Libros/9788499890951.jpg'},
+      {titulo: '1984', autor: 'George Orwell', paginas: 432, isbn: '978-8490328248', imagen: 'Resources/img/Libros/978-8490328248.jpg', frase: '"El progreso tecnológico se permite solo cuando sus productos pueden aplicarse de algún modo a disminuir la libertad humana."'},
+      {titulo: 'Rebelion en la granja', autor: 'George Orwell', paginas: 144, isbn: '9788499890951', imagen: 'Resources/img/Libros/9788499890951.jpg', frase: '"Si algo significa la libertad es el derecho a decirle a la gente lo que no quiere oír"'},
     ],    
   },
 
