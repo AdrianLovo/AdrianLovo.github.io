30bb6796ce0fde6add575668604e7b27ef98239d
AdrianLovo <adrianhumbertocastillo@gmail.com>
2019-08-07 18:09:44 -0600

    Modificado archivo de librs

--- Resources/js/Libros.js
+++ Resources/js/Libros.js
@@ -3,8 +3,9 @@ var vm = new Vue({
   el: 'main', 
   data: {
     listado: [
-      {titulo: '1984', autor: 'George Orwell', paginas: 432, isbn: '978-8490328248', imagen: 'Resources/img/Libros/978-8490328248.jpg', frase: '"El progreso tecnológico se permite solo cuando sus productos pueden aplicarse de algún modo a disminuir la libertad humana."'},
-      {titulo: 'Rebelion en la granja', autor: 'George Orwell', paginas: 144, isbn: '9788499890951', imagen: 'Resources/img/Libros/9788499890951.jpg', frase: '"Si algo significa la libertad es el derecho a decirle a la gente lo que no quiere oír"'},
+      {titulo: '1984', autor: 'George Orwell', imagen: 'Resources/img/Libros/978-8490328248.jpg', frase: '"El progreso tecnológico se permite solo cuando sus productos pueden aplicarse de algún modo a disminuir la libertad humana."'},
+      {titulo: 'Rebelion en la granja', autor: 'George Orwell', imagen: 'Resources/img/Libros/9788499890951.jpg', frase: '"Si algo significa la libertad es el derecho a decirle a la gente lo que no quiere oír"'},
+      {titulo: 'Fahrenheit 451', autor: 'Ray Bradbury', imagen: 'Resources/img/Libros/9788490321478.jpg', frase: '"No hace falta quemar libros si el mundo empieza a llenarse de gente que no lee, que no aprende, que no sabe"'},
     ],    
   },
 
