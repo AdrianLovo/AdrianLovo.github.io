26c87d4a2bfb05913cf77e1042d145861cfc2194
AdrianLovo <adrianhumbertocastillo@gmail.com>
2019-08-12 16:47:24 -0600

    Agregado libro al listado

--- Resources/js/Libros.js
+++ Resources/js/Libros.js
@@ -6,6 +6,7 @@ var vm = new Vue({
       {titulo: '1984', autor: 'George Orwell', imagen: 'Resources/img/Libros/978-8490328248.jpg', frase: '"El progreso tecnológico se permite solo cuando sus productos pueden aplicarse de algún modo a disminuir la libertad humana."'},
       {titulo: 'Rebelion en la granja', autor: 'George Orwell', imagen: 'Resources/img/Libros/9788499890951.jpg', frase: '"Si algo significa la libertad es el derecho a decirle a la gente lo que no quiere oír"'},
       {titulo: 'Fahrenheit 451', autor: 'Ray Bradbury', imagen: 'Resources/img/Libros/9788490321478.jpg', frase: '"No hace falta quemar libros si el mundo empieza a llenarse de gente que no lee, que no aprende, que no sabe"'},
+      {titulo: 'El fin de la infancia', autor: 'Arthur C. Clarke', imagen: 'Resources/img/Libros/9781480535541.jpg', frase: '"Una raza extraña llega a la tierra y trae consigo la paz, prosperidad y la inesperada tragedia de la perfección"'},
     ],    
   },
 
