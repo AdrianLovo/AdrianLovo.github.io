76c749394ff8d97a6f8ac78419a6a954437e1f9f
AdrianLovo <adrianhumbertocastillo@gmail.com>
2019-08-17 17:12:05 -0600

    Agregado libro a listado de libros

--- Resources/js/Libros.js
+++ Resources/js/Libros.js
@@ -8,6 +8,7 @@ var vm = new Vue({
       {titulo: 'Fahrenheit 451', autor: 'Ray Bradbury', imagen: 'Resources/img/Libros/9788490321478.jpg', frase: '"No hace falta quemar libros si el mundo empieza a llenarse de gente que no lee, que no aprende, que no sabe"'},
       {titulo: 'El fin de la infancia', autor: 'Arthur C. Clarke', imagen: 'Resources/img/Libros/9781480535541.jpg', frase: '"Una raza extraña llega a la tierra y trae consigo la paz, prosperidad y la inesperada tragedia de la perfección"'},
       {titulo: 'Yo Robot', autor: 'Isaac Asimov', imagen: 'Resources/img/Libros/9788073874919.jpg', frase: '“Si fuese posible crear un robot capaz de ser funcionario civil, creo que haríamos un gran bien, ya que las Leyes de la Robótica le impedirían dañar a un ser humano, lo incapacitarían para la tiranía, la corrupción, la estupidez, el prejuicio.”'},
+      {titulo: 'El extraño caso del Dr. Jekyll y Mr. Hyde', autor: 'Robert Louis Stevenson', imagen: 'Resources/img/Libros/9780460007672.jpg', frase: '“El bien y el mal se encuentran tan cercca que están encadenados en el alma”'},
     ],    
   },
 
