127054d2045db68988e96aec2dac55b4b8f80b93
AdrianLovo <adrianhumbertocastillo@gmail.com>
2019-08-16 21:50:25 -0600

    Agregado libro y frase Yo Robot en la pagina de libros

--- Resources/js/Libros.js
+++ Resources/js/Libros.js
@@ -7,9 +7,11 @@ var vm = new Vue({
       {titulo: 'Rebelion en la granja', autor: 'George Orwell', imagen: 'Resources/img/Libros/9788499890951.jpg', frase: '"Si algo significa la libertad es el derecho a decirle a la gente lo que no quiere oír"'},
       {titulo: 'Fahrenheit 451', autor: 'Ray Bradbury', imagen: 'Resources/img/Libros/9788490321478.jpg', frase: '"No hace falta quemar libros si el mundo empieza a llenarse de gente que no lee, que no aprende, que no sabe"'},
       {titulo: 'El fin de la infancia', autor: 'Arthur C. Clarke', imagen: 'Resources/img/Libros/9781480535541.jpg', frase: '"Una raza extraña llega a la tierra y trae consigo la paz, prosperidad y la inesperada tragedia de la perfección"'},
+      {titulo: 'Yo Robot', autor: 'Isaac Asimov', imagen: 'Resources/img/Libros/9788073874919.jpg', frase: '“Si fuese posible crear un robot capaz de ser funcionario civil, creo que haríamos un gran bien, ya que las Leyes de la Robótica le impedirían dañar a un ser humano, lo incapacitarían para la tiranía, la corrupción, la estupidez, el prejuicio.”'},
     ],    
   },
 
 
 })
+
 
